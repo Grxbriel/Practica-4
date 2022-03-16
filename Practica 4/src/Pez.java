fbe96cd738e48fff234de3f1c55bc1a2ffb5b7bf
Gabi <87373226+Grxbriel@users.noreply.github.com>
2022-03-16 21:06:42 +0100

    2.1.0

---
+++ Practica 4/src/Pez.java
@@ -0,0 +1,29 @@
+public class Pez extends Mascota {
+
+    //Atributos
+    private String raza ;
+
+    //Constructores
+    public Pez(String raza){
+        super(Nombre, Edad);
+        this.setRaza(raza);
+    }
+    public Pez(){
+        super();
+        this.raza = null;
+    }
+
+    //toString
+    @Override
+	public String toString() {
+		return "Pez [raza=" + raza + ", getNombre()=" + getNombre() + ", getEdad()=" + getEdad() + "]";
+	}
+
+    //Getters y Setters
+    public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+}
